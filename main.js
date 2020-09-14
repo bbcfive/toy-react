c0a9979f3a0f5d8176d624c98407124137b7d083
bbcfive <bbcfive@163.com>
2020-09-14 22:42:14 +0800

    realize jsx

---
+++ main.js
@@ -0,0 +1,20 @@
+import { createElement, Component, render } from './toy-react';
+ 
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>my component</h1>
+        {this.children}
+      </div>
+    );
+  };
+};
+
+
+render(<MyComponent id="a" class="c">
+    <div>abc</div>
+    <div>def</div>
+    <div>ghi</div>
+  </MyComponent>, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {
+    main: './main.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}]]
+          }
+        }
+      }
+    ]
+  }
+}
