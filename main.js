c6b44124fa76127929fe2c2f635c5ae0f3df6a50
bbcfive <bbcfive@163.com>
2020-09-16 00:04:43 +0800

    add rerender to update component state

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ import { createElement, Component, render } from './toy-react';
  
 
 class MyComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
   render() {
     return (
       <div>
         <h1>my component</h1>
         {this.children}
+        <button onclick={() => {this.setState({a: this.state.a+1})}}>add</button>
+        <div> 
+          {this.state.a.toString()}
+        </div>
       </div>
     );
   };
--- toy-react.js
+++ toy-react.js
@@ -1,27 +1,45 @@
+const RENDER_TO_DOM = Symbol('render to dom');
 
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value);
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+    } else if (name === "className") {
+      this.root.setAttribute('class', value);
+    } else {    
+      this.root.setAttribute(name, value);
+    }
   }
   appendChild(component) {
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
   }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+  };
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
   }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+  };
 }
 
 export class Component {
   constructor() {
     this.props = Object.create(null);
     this.children = [];
-    this._root = null;
+    this._range = null;
   }
   setAttribute(name, value) {
     this.props[name] = value;
@@ -29,11 +47,40 @@ export class Component {
   appendChild(component) {
     this.children.push(component);
   }
-  get root () {
-    if (!this._root) {
-      this._root = this.render().root;
+  [RENDER_TO_DOM](range) {
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  };
+  rerender() {
+    let oldRange = this._range;
+
+    let range = document.createRange();
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    this[RENDER_TO_DOM](range);
+    
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== "object") {
+      this.state = newState;
+      this.rerender();
+      return;
     }
-    return this._root;
+
+    let merge = (oldState, newState) => {
+      for(let p in oldState) {
+        if (oldState[p] === null || typeof oldState[p] !== "object") {
+          oldState[p] = newState[p];
+        } else {
+          merge(oldState[p], newState[p]);
+        } 
+      }
+    }
+
+    merge(this.state, newState);
+    this.rerender();
   }
 }
 
@@ -45,14 +92,17 @@ export function createElement(type, attributes, ...children) {
     e = new type;
   }
 
-  for (let attribute in attributes) {
-    e.setAttribute(attributes[attribute]);
+  for (let p in attributes) {
+    e.setAttribute(p, attributes[p]);
   }
 
   let insertChildren = (children) => {
     for (let child of children) {
       if (typeof child === "string") {
         child = new TextWrapper(child);
+      } 
+      if (child === null) {
+        continue;
       }
       if (typeof child === "object" && child instanceof Array) {
         insertChildren(child);
@@ -66,6 +116,10 @@ export function createElement(type, attributes, ...children) {
   return e;
 }
 
-export function render(element, parentElement) {
-  return parentElement.appendChild(element.root);
+export function render(component, parentElement) {
+  const range = document.createRange();
+  range.setStart(parentElement, 0);
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+  component[RENDER_TO_DOM](range);
 }
